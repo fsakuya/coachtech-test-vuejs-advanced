c3d122663d0ac2b9c442fb375f81c855b9b15218
fsakuya <futamori.1991@gmail.com>
2023-04-16 18:23:00 +0900

    first-commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="about">
+    <dir>current team: {{ }}</dir>
+    <button>{{ clickCount }}</button>
+    <div>{{ }}</div>
+    <div>Last team name clicked: {{}}</div>
+    <button>戻る</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['itemId'],
+  data() {
+    return {
+      teams: [
+        {
+          id: "c36136ef-552f-4dc9-84b6-65ecb8574e81",
+          name: "Team_A",
+        },
+        {
+          id: "ed0459f5-716d-4ef4-99ba-5dc750dfe0d0",
+          name: "Team_B",
+        },
+        {
+          id: "60d0d5d5-8ce9-41d9-a65b-13a090c10ae1",
+          name: "Team_C",
+        },
+      ],
+    };
+  },
+
+};
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="home">
+    <h2>チーム一覧</h2>
+    <div v-for="team in teams" :key="team.id">
+      <router-link v-bind:to="{ name: 'about', params: {teamId: id} }">
+        {{ team.name }}
+      </router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeTest",
+  data() {
+    return {
+      teams: [
+        {
+          id: "c36136ef-552f-4dc9-84b6-65ecb8574e81",
+          name: "Team_A",
+        },
+        {
+          id: "ed0459f5-716d-4ef4-99ba-5dc750dfe0d0",
+          name: "Team_B",
+        },
+        {
+          id: "60d0d5d5-8ce9-41d9-a65b-13a090c10ae1",
+          name: "Team_C",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+div {
+  line-height: 30px;
+}
+</style>
